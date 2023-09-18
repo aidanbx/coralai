f95c93f84c8b8fa6512969b50db48f6bedf770ba
Aidan Barbieux <aidanbx@gmail.com>
2023-09-17 18:17:10 -0700

    grow_muscle works on env rather than batch

--- src/physics.py
+++ src/physics.py
@@ -2,100 +2,48 @@ import torch
 import numpy as np
 from typing import Tuple
 
-def grow_muscle(radii: torch.Tensor, radii_deltas: torch.Tensor,
-                capital: torch.Tensor, efficiency: torch.Tensor
+def grow_muscle_csa(muscle_radii: torch.Tensor, radii_deltas: torch.Tensor,
+                capital: torch.Tensor, growth_efficiency: torch.Tensor
                 ) -> Tuple[torch.Tensor, torch.Tensor]:
+    # TODO: Update to work on batches of environments (simple)
     """
     Function to simulate the growth of muscle tissue. Modifies the radii and capital tensors in-place.
 
     Parameters:
-    radii (torch.Tensor): The radii of the muscles of each cell. Shape: (batch_size, num_muscles)
-    radii_deltas (torch.Tensor): Desired changes in radii. Shape: (batch_size, num_muscles)
-    capital (torch.Tensor): capital present on each cell. Shape: (batch_size,)
-    efficiency (torch.Tensor): The efficiency (heat loss) for a given change in muscle size. Shape: (batch_size,)
+    radii (torch.Tensor): The radii of the muscles of each cell. Shape: (kernel_size, width, height)
+    radii_deltas (torch.Tensor): Desired changes in radii. Shape: (kernel_size, width, height)
+    capital (torch.Tensor): capital present on each cell. Shape: (width, height)
+    efficiency (torch.Tensor): The efficiency (heat loss) for a given change in muscle size. Shape: (width, height)
 
     Returns:
-    Tuple[torch.Tensor, torch.Tensor]: The new muscle radii and capital for
-      each cell. Shape: ((batch_size, num_muscles), (batch_size,))
+    Tuple[torch.Tensor, torch.Tensor]: The new muscle radii and capital tensors.
     """
     zero_tensor = torch.tensor(0.0)
-    csa_deltas = (radii + radii_deltas)**2 - radii**2  # cross-sectional area
+    csa_deltas = (muscle_radii + radii_deltas)**2 - muscle_radii**2  # cross-sectional area
+    print(csa_deltas[[1,2]])
 
     negative_csa_deltas = torch.where(csa_deltas < 0, csa_deltas, zero_tensor)
     positive_csa_deltas = torch.where(csa_deltas > 0, csa_deltas, zero_tensor)
 
     # Atrophy muscle and convert to capital
-    capital.sub_(torch.sum(negative_csa_deltas, dim=1) * efficiency)
-    
-    new_csa_mags = radii**2.0
+    capital.sub_(torch.sum(negative_csa_deltas, dim=0) * growth_efficiency)
+
+    new_csa_mags = muscle_radii**2.0
     new_csa_mags.add_(negative_csa_deltas)
 
     # Grow muscle from capital, if possible
-    capital_desired = torch.sum(positive_csa_deltas, dim=1)
+    capital_desired = torch.sum(positive_csa_deltas, dim=0)
     capital_desired_safe = torch.where(capital_desired == 0, torch.tensor(1.0), capital_desired)
-    csa_delta_distribution = positive_csa_deltas / capital_desired_safe.unsqueeze(1)
+    csa_delta_distribution = positive_csa_deltas / capital_desired_safe.unsqueeze(0) 
+    del capital_desired_safe # TODO: Free CUDA memory? Garbage Collect?  
 
     capital_consumed = torch.where(capital_desired > capital, capital, capital_desired)
     capital.sub_(capital_consumed)
-    capital_consumed.mul_(efficiency)
-    new_csa_mags.addcmul_(capital_consumed.unsqueeze(1), csa_delta_distribution)
-    torch.mul(torch.sqrt(new_csa_mags), torch.sign(radii + radii_deltas), out=radii)  
+    capital_consumed.mul_(growth_efficiency)
+    new_csa_mags.addcmul_(capital_consumed.unsqueeze(0), csa_delta_distribution)
+    torch.mul(torch.sqrt(new_csa_mags), torch.sign(muscle_radii + radii_deltas), out=muscle_radii)  
 
-    return radii, capital
-
-
-def grow_old(rads_b, rad_deltas_b, capital_b, efficiency_b):
-    csa_deltas_b = (rads_b + rad_deltas_b)**2 - rads_b**2 # cross-sectional area
-
-    # Atrophy muscle and convert to capital
-    capital_b -= torch.sum(torch.where(csa_deltas_b < 0, csa_deltas_b, torch.tensor(0.0)),dim=1) * efficiency_b
-
-    new_csa_mags_b = rads_b**2.0
-    new_csa_mags_b[csa_deltas_b < 0] += csa_deltas_b[csa_deltas_b < 0]
-
-    # Grow myscle from capital, if possible
-    capital_desired_b = torch.sum(torch.where(csa_deltas_b > 0, csa_deltas_b, torch.tensor(0.0)),dim=1)
-    csa_delta_distribution_b = torch.where(csa_deltas_b > 0, csa_deltas_b, torch.tensor(0.0)) / capital_desired_b.unsqueeze(1)
-
-    capital_consumed_b = torch.where(capital_desired_b > capital_b, capital_b, capital_desired_b)
-    csa_grown_b = capital_consumed_b * efficiency_b
-    new_csa_mags_b = torch.where(csa_deltas_b > 0, new_csa_mags_b + csa_grown_b.unsqueeze(1) * csa_delta_distribution_b, new_csa_mags_b)
-
-    capital_b -= capital_consumed_b
-
-    new_rad_mags_b = torch.sqrt(new_csa_mags_b)
-    new_signs = torch.sign(rads_b + rad_deltas_b)
-
-    return new_rad_mags_b * new_signs, capital_b
-
-
-def grow_dumb(rads, rad_deltas, capital, efficiency):
-    activate_muscle_growth = np.vectorize(lambda rads, rad_deltas: (rads + rad_deltas)**2 - rads**2)
-    csa_deltas = activate_muscle_growth(rads, rad_deltas) # cross-sectional area
-    positive_csa_deltas = csa_deltas[csa_deltas > 0]
-    negative_csa_deltas = csa_deltas[csa_deltas < 0]
-
-    # Atrophy muscle and convert to capital
-    capital -= sum(negative_csa_deltas) * efficiency
-    new_csa_mags = rads**2.0
-    new_csa_mags[csa_deltas < 0] += negative_csa_deltas
-
-    # Grow muscle from capital, if possible
-    capital_desired = sum(positive_csa_deltas) # before efficiency loss
-    csa_delta_distribution = positive_csa_deltas / capital_desired
-
-    if capital_desired > capital:
-        capital_consumed = capital
-    else:
-        capital_consumed = capital_desired
-
-    csa_grown = capital_consumed * efficiency
-    new_csa_mags[csa_deltas > 0] += csa_grown * csa_delta_distribution
-
-    capital -= capital_consumed
-
-    new_rad_mags = np.sqrt(new_csa_mags)
-    new_signs = np.sign(rads + rad_deltas)
+    return muscle_radii, capital
 
 
 if __name__ == "__main__":
@@ -111,7 +59,7 @@ if __name__ == "__main__":
     radii_results = []
     capital_results = []
     for _ in range(10):
-        out = grow_muscle(rads_batch, rad_deltas_batch, capital_batch, efficiency_batch)
+        out = grow_muscle_csa(rads_batch, rad_deltas_batch, capital_batch, efficiency_batch)
         radii_results.append(out[0].item())
         capital_results.append(out[1].item())
 
