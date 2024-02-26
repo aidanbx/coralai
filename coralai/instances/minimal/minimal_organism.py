0e881847e2b8ecbe7ec64dc3b0deb6d9f01c72a5
Aidan Barbieux <aidanbx@gmail.com>
2024-02-26 14:41:01 -0500

    truly minimal organism now

--- coralai/instances/minimal/minimal_organism.py
+++ coralai/instances/minimal/minimal_organism.py
@@ -7,36 +7,12 @@ from ...dynamics.organism import Organism
 
 @ti.data_oriented
 class MinimalOrganism(Organism):
-    def __init__(self, n_sensors, n_actuators, torch_device, latent_size = None):
+    def __init__(self, n_sensors, n_actuators, torch_device):
         super().__init__(n_sensors, n_actuators)
-
-        if latent_size is None:
-            latent_size = (self.n_sensors + self.n_actuators) // 2
-        self.latent_size = latent_size
 
         # First convolutional layer
         self.conv = nn.Conv2d(
             self.n_sensors,
-            self.latent_size,
-            kernel_size=3,
-            padding=1,
-            padding_mode='circular',
-            device=torch_device,
-            bias=False
-        )
-
-        self.latent_conv = nn.Conv2d(
-            self.latent_size,
-            self.latent_size,
-            kernel_size=3,
-            padding=1,
-            padding_mode='circular',
-            device=torch_device,
-            bias=False
-        )
-
-        self.latent_conv_2 = nn.Conv2d(
-            self.latent_size,
             self.n_actuators,
             kernel_size=3,
             padding=1,
@@ -44,20 +20,11 @@ class MinimalOrganism(Organism):
             device=torch_device,
             bias=False
         )
+
     
     def forward(self, x):
         with torch.no_grad():
             x = self.conv(x)
-            x = nn.ReLU()(x)
-            x = ch_norm(x)
-            x = torch.sigmoid(x)
-
-            x = self.latent_conv(x)
-            x = nn.ReLU()(x)
-            x = ch_norm(x)
-            x = torch.sigmoid(x)
-
-            x = self.latent_conv_2(x)
             x = nn.ReLU()(x)
             x = ch_norm(x)
             x = torch.sigmoid(x)
@@ -67,5 +34,4 @@ class MinimalOrganism(Organism):
 
     def perturb_weights(self, perturbation_strength):
         self.conv.weight.data += perturbation_strength * torch.randn_like(self.conv.weight.data)
-        self.latent_conv.weight.data += perturbation_strength * torch.randn_like(self.latent_conv.weight.data)
-        self.latent_conv_2.weight.data += perturbation_strength * torch.randn_like(self.latent_conv_2.weight.data)
+        
