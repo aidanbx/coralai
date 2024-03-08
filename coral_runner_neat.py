988e3208ee2c68c29aa8ccc58ab3e3c8e664ccd0
Aidan Barbieux <aidanbx@gmail.com>
2024-03-08 01:46:25 -0800

    Lots of tweaks to physics etc., evolving and saving runs

--- coral_runner_neat.py
+++ coral_runner_neat.py
@@ -1,35 +1,61 @@
 import os
 import torch
+import neat
 import taichi as ti
-from coralai.instances.coral.coral_physics import apply_physics
 from coralai.substrate.substrate import Substrate
 from coralai.evolution.neat_evolver import NEATEvolver
 from coralai.substrate.visualization import Visualization
 
 class CoralVis(Visualization):
-    def __init__(self, substrate, ecosystem, vis_chs):
+    def __init__(self, substrate, evolver, vis_chs):
         super().__init__(substrate, vis_chs)
-        self.ecosystem = ecosystem
+        self.evolver = evolver
+        self.next_generation = False
+        self.genome_stats = []
+
 
     def render_opt_window(self):
         inds = self.substrate.ti_indices[None]
         self.canvas.set_background_color((1, 1, 1))
-        opt_w = min(480 / self.img_w, self.img_w)
-        opt_h = min(340 / self.img_h, self.img_h * 2)
+        opt_w = min(200 / self.img_w, self.img_w)
+        opt_h = min(500 / self.img_h, self.img_h * 2)
         with self.gui.sub_window("Options", 0.05, 0.05, opt_w, opt_h) as sub_w:
-            self.opt_window(sub_w)
+            # self.opt_window(sub_w)
+            self.next_generation = sub_w.checkbox("Next Generation", self.next_generation)
+            self.chinds[0] = sub_w.slider_int(
+                f"R: {self.substrate.index_to_chname(self.chinds[0])}", 
+                self.chinds[0], 0, self.substrate.mem.shape[1]-1)
+            self.chinds[1] = sub_w.slider_int(
+                f"G: {self.substrate.index_to_chname(self.chinds[1])}", 
+                self.chinds[1], 0, self.substrate.mem.shape[1]-1)
+            self.chinds[2] = sub_w.slider_int(
+                f"B: {self.substrate.index_to_chname(self.chinds[2])}", 
+                self.chinds[2], 0, self.substrate.mem.shape[1]-1)
             current_pos = self.window.get_cursor_pos()
             pos_x = int(current_pos[0] * self.w) % self.w
             pos_y = int(current_pos[1] * self.h) % self.h
-            sub_w.text(f"Stats at ({pos_x}, {pos_y}):")
             sub_w.text(
-                f"Energy: {self.substrate.mem[0, inds.energy, pos_x, pos_y]:.2f}," +
-                f"Infra: {self.substrate.mem[0, inds.infra, pos_x, pos_y]:.2f}," +
-                f"Genome: {self.substrate.mem[0, inds.genome, pos_x, pos_y]:.2f}, " 
+                f"GENOME: {self.substrate.mem[0, inds.genome, pos_x, pos_y]:.2f}\n" +
+                f"Energy: {self.substrate.mem[0, inds.energy, pos_x, pos_y]:.2f}\n" +
+                f"Infra: {self.substrate.mem[0, inds.infra, pos_x, pos_y]:.2f}\n"
                 # f"Acts: {self.substrate.mem[0, inds.acts, pos_x, pos_y]}"
             )
-            sub_w.text(f"Total Energy: {torch.sum(self.substrate.mem[0, inds.energy])}")
-            sub_w.text(f"Energy Offset: {self.ecosystem.energy_offset}")
+            tot_energy = torch.sum(self.substrate.mem[0, inds.energy])
+            tot_infra = torch.sum(self.substrate.mem[0, inds.infra])
+            sub_w.text(f"Total Energy+Infra: {tot_energy + tot_infra}")
+            sub_w.text(f"Percent Energy: {(tot_energy / (tot_energy + tot_infra)) * 100}")
+            sub_w.text(f"Energy Offset: {self.evolver.energy_offset}")
+            sub_w.text("# Infra in Genome:")
+
+            if self.evolver.timestep % 20 == 0:
+                self.genome_stats = []
+                for i in range(len(self.evolver.organisms)):
+                    # n_cells = self.substrate.mem[0, inds.genome].eq(i).sum().item()
+                    n_cells = self.evolver.get_genome_infra_sum(i)
+                    self.genome_stats.append((i, n_cells))
+                self.genome_stats.sort(key=lambda x: x[1], reverse=True)
+            for i, n_cells in self.genome_stats:
+                sub_w.text(f"  {i}: {n_cells:.2f}")
 
 
 def main(config_filename, channels, shape, kernel, ind_of_middle, sense_chs, act_chs, torch_device):
@@ -39,14 +65,21 @@ def main(config_filename, channels, shape, kernel, ind_of_middle, sense_chs, act
     substrate = Substrate(shape, torch.float32, torch_device, channels)
     substrate.malloc()
 
+    inds = substrate.ti_indices[None]
+
     neat_evolver = NEATEvolver(config_path, substrate, kernel, ind_of_middle, sense_chs, act_chs,)
     
     def eval_vis(genomes, config):
         vis = CoralVis(substrate, neat_evolver, ["energy", "infra", "genome"])
         import random
-        random_steps = random.randint(100, 500)
-        neat_evolver.eval_genomes(genomes, config, random_steps, vis)
-    neat_evolver.population.run(eval_vis, 100)
+        random_steps = random.randint(500, 1000)
+        neat_evolver.eval_genomes(genomes, random_steps, vis)
+    
+    neat_evolver.gen_population()
+    neat_evolver.population.run(eval_vis, 500)
+    # checkpoint_file = os.path.join('history', 'NEAT_240308-0052_32', 'checkpoint4')
+    # p = neat.Checkpointer.restore_checkpoint(checkpoint_file)
+    # p.run(eval_vis, 10)
 
 
 if __name__ == "__main__":
@@ -55,7 +88,6 @@ if __name__ == "__main__":
     main(
         config_filename = "coralai/instances/coral/coral_neat.config",
         channels = {
-            "genome": ti.f32,
             "energy": ti.f32,
             "infra": ti.f32,
             "acts": ti.types.struct(
@@ -69,12 +101,13 @@ if __name__ == "__main__":
                 c=ti.f32,
                 d=ti.f32
             ),
+            "genome": ti.f32,
         },
         shape = (200, 200),
-        kernel = [        [0,-1],
-                  [-1, 0],[0, 0],[1, 0],
-                          [0, 1],        ],
-        ind_of_middle = 2,
+        kernel = [[0, 0], [0, -1], [0, 1],
+                  [1, 0], [1, -1], [1, 1],
+                  [-1, 0],[-1,-1],[-1, 1]],
+        ind_of_middle = 0,
         sense_chs = ['energy', 'infra', 'com'],
         act_chs = ['acts', 'com'],
         torch_device = torch_device
